ad532ffe5c5de07ddd72c98cdfd06bd6b6ecd497
Sydney Chapman <chapman.sydney44@bloomfield.org>
2018-07-16 15:56:02 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,29 @@
 # Code your cash register here!
+
+class AmazonTransaction
+end
+
+amazon_transaction = AmazonTransaction.new 
+amazon_transaction_with_discount = AmazonTransaction.new 
+
+ 
+ def initialize(total)
+   @total = total 
+  end
+  
+def initialize
+   @total = 0 
+   @items = []
+   @discount = 20 
+   end
+    
+    def total
+      @total
+    end 
+    
+    amazon_transaction.total = 100
+    
+    
+    
+      
+  
